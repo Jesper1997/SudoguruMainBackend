a20b492f7563377b7f9452448ef1982483db8991
jesper <jesper.jansen@student.fontys.nl>
2021-10-11 09:20:23 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ SudoguruMainBackend/ViewModels/SquareViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudoguruMainBackend.ViewModels
+{
+    public class SquareViewModel
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int value { get; set; }
+        public int id { get; set; }
+
+    }
+}
